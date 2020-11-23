67dfe9c891b324e52ae59c8cdc99411ee8fd6ee5
Abhik Roy <olgratefuldead@gmail.com>
2020-11-23 01:13:35 -0500

    edp613

--- static/slides/Week 11/xaringanExtractR.R
+++ static/slides/Week 11/xaringanExtractR.R
@@ -3,5 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(knitr)
 
-purl("Slides-Week-10.Rmd")
-purl("Slides-Week-10R.Rmd")
+purl("Slides-Week-11.Rmd")
+purl("Slides-Week-11R.Rmd")
