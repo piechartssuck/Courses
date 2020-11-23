57adf7043cf13fe924efc79eb8d8bb496a351b9f
Abhik Roy <olgratefuldead@gmail.com>
2020-11-23 01:30:45 -0500

    edp613

--- public/slides/Week 11/xaringanPDF.R
+++ public/slides/Week 11/xaringanPDF.R
@@ -1,6 +1,6 @@
 # Set the working directory as source ----
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-pagedown::chrome_print("Slides-Week-11.Rmd")
-pagedown::chrome_print("Slides-Week-11R.Rmd")
+pagedown::chrome_print("Slides-Week-11.Rmd", format = "pdf", timeout = 90)
+pagedown::chrome_print("Slides-Week-11R.Rmd", format = "pdf", timeout = 90)
 
