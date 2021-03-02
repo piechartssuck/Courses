68c7d57967c06b1f18391dc7d766bf2e0f76d89c
piechartssuck <>
2021-03-02 14:02:25 -0500

    edp693e

--- public/assignment/06-assignment.R
+++ public/assignment/06-assignment.R
@@ -1,10 +1,4 @@
-## ----setup, include=FALSE---------------------------------------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
 
-
-
-
-## ----message=FALSE----------------------------------------------------------------------------------------------
 library(ggmap) # Provides the ability to visualize spatial data
 library(maps) # Computes the areas of regions in a projected map
 library(mapdata) # Providing both larger and higher-resolution databases.
@@ -13,123 +7,110 @@ library(rgdal) # Bindings for the Geospatial Data Abstraction Library
 library(tidyverse)
 library(tools) # Core package for R and is worth noting
 library(viridis)
-
-
-## ---------------------------------------------------------------------------------------------------------------
 library(censusapi)
 
+Sys.setenv(CENSUS_KEY="YOUR API KEY HERE")
 
-
-
-## ----eval=FALSE, echo=TRUE--------------------------------------------------------------------------------------
-## Sys.setenv(CENSUS_KEY="YOUR API KEY HERE")
-
-
-## ---------------------------------------------------------------------------------------------------------------
 readRenviron("~/.Renviron") # You may get a warning...ignore it
 
-
-## ---- eval=FALSE------------------------------------------------------------------------------------------------
-## Sys.getenv("CENSUS_KEY")
+Sys.getenv("CENSUS_KEY")
 
 
-## ---------------------------------------------------------------------------------------------------------------
 usa <- map_data("usa")
 
 
-## ---------------------------------------------------------------------------------------------------------------
 dim(usa)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(usa) 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tail(usa) 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = usa, 
                aes(x=long, y = lat, group = group))
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = usa, 
                aes(x=long, y = lat, group = group)) + 
   coord_fixed(1.3) # 1.3 is a decent standard estimate
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = usa, aes(x=long, y = lat, group = group), color = "#1b85b8", 
                fill = "#559e83") + 
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 state <- map_data("state")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 California <- map_data("state", region = "CA")
 
 
-## ---- error = TRUE----------------------------------------------------------------------------------------------
+
 Kansas <- map_data("state", region = "KS")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Michigan <- map_data("state", region = "MI")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 unique(Michigan$region)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Michigan <- map_data("state", 
                      region = "Michigan")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 unique(Michigan$region)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 mi <- subset(state, 
              region=="michigan")
 
 
-## ---- out.width = "47%"-----------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = mi, 
                         aes(x=long, y = lat, group = group)) + 
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = mi, aes(x=long, y = lat, group = group, fill=region)) +
   scale_fill_manual(values=c("#6A0032")) +
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 county <- map_data("county")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 mi_count <- subset(county, region=="michigan")
 
 
-## ---- include = FALSE-------------------------------------------------------------------------------------------
+
 par(mar = c(12, 6, 4, 2) + 0.1)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = mi_count, aes(x=long, y = lat, group = group, fill=subregion), 
                color = "#FFFFFF") +
@@ -157,7 +138,7 @@ ggplot() +
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = mi_count, aes(x=long, y = lat, group = group, fill=subregion), 
                color = "#FFFFFF") +
@@ -187,49 +168,49 @@ ggplot() +
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 apis <- listCensusApis()
 
 
-## ---- echo=TRUE,eval=FALSE--------------------------------------------------------------------------------------
-## View(apis)
+
+View(apis)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 acs2017_vars <- listCensusMetadata(name = "2017/pep/population/", 
                                  type = "variables")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(acs2017_vars)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 acs2017_geos <- listCensusMetadata(name = "2017/pep/population/", 
                                    type = "geography")
 
 
-## ----echo=TRUE,eval=FALSE---------------------------------------------------------------------------------------
-## View(acs2017_geos)
+
+View(acs2017_geos)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 library(rvest)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 webpage <- read_html("https://api.census.gov/data/2017/pep/population/variables.html")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tbls <- html_nodes(webpage, "table") 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tbls # or if there are multiple tables, consider using head(tbls)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tbls_ls <- webpage %>%
         html_nodes("table") %>% # Creates a multidimensional list (i.e. a list 
                                 # with layers and depth akin to folders within 
@@ -241,32 +222,29 @@ tbls_ls <- webpage %>%
                                 # the entries
 
 
-## ---- echo=TRUE,eval=FALSE--------------------------------------------------------------------------------------
-## View(tbls_ls)
+
+View(tbls_ls)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tbls_ls <- do.call(rbind.data.frame, tbls_ls) # Concatenates a list of data frames 
                                               # into a single data frame
 
 
-## ----echo=TRUE,eval=FALSE---------------------------------------------------------------------------------------
-## View(tbls_ls)
+
+View(tbls_ls)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 filtered_tbls_ls <- tbls_ls[tbls_ls$Required == "default displayed", ] 
                                                               # Filter out the rows that
                                                               # don't have `default 
                                                               # displayed` under the 
                                                               # column **Required**
 
-
-## ---------------------------------------------------------------------------------------------------------------
 filtered_tbls_ls$Name # Just shows the Names of the required variables
 
 
-## ---------------------------------------------------------------------------------------------------------------
 library(rvest)
 
 read_html("https://api.census.gov/data/2017/pep/population/variables.html") %>%
@@ -284,11 +262,11 @@ html_nodes("table")  %>% # Creates a multidimensional list (i.e. a list with lay
   select(Name) # Just shows the Names of the required variables
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 tbls_ls$Name
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 read_html("https://api.census.gov/data/2017/pep/population/variables.html") %>%
   html_nodes("table")  %>% # This is a multidimensional list (i.e. a list with layers)
   .[1] %>% 
@@ -297,109 +275,109 @@ read_html("https://api.census.gov/data/2017/pep/population/variables.html") %>%
   select(Name)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 getCensus(name = "2017/pep/population",
           vars = c("DENSITY"), 
           region = "state:26")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Tigers <- getCensus(name = "2017/pep/population",
                          vars = c("GEONAME", "COUNTY", "DENSITY"), 
                          region = "county:*",
                          regionin = "state:26")
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers, n=15L)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 drops <- c("state", "county", "COUNTY") # Assigns column names to be dropped
 Tigers <- Tigers[ , !(names(Tigers) %in% drops)] # Drops those columns 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Tigers <- Tigers %>% 
   separate(GEONAME, into = paste0('thing', 1:2), sep = '[,]')
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Tigers <- setNames(Tigers, c("County","State","Density")) 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 Tigers$County <- str_replace_all(Tigers$County, " County", "") 
 # Find the term County in the County column and replace it nothing (given by ""). Notice 
 # that there is a space between the first quote and the term County to account for the 
 # space between the two words.
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 dropFromCensus <- c("State")
 Tigers <- Tigers[ , !(names(Tigers) %in% dropFromCensus)] # Drops those columns
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 dropFromMapData <- c("group", "order", "region")
 mi_count <- mi_count[ , !(names(mi_count) %in% dropFromMapData)] # Drops those columns
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 mi_count <- setNames(mi_count, c("long","lat","County")) 
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 mi_count$County <- str_to_title(mi_count$County)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(Tigers)
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 total_thing <- left_join(mi_count, Tigers, by = c("County"))
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 head(mi_count)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = total_thing, aes(x=long, y = lat, fill=Density, group=County), 
                color = "#f8f8fa", size = 0.25, show.legend = T) +
@@ -415,7 +393,6 @@ ggplot() +
         legend.title.align = 0.5,
         legend.spacing.x = unit(0.5, "cm"), 
         legend.spacing.y = unit(0.5, "cm"),
-        # legend.box.margin = unit(c(0,0,0,0),"cm"),
         legend.background = element_rect(linetype = 0, size = 0.5, colour = 1),
         panel.background = element_blank(),
         axis.title.x = element_blank(),
@@ -428,11 +405,11 @@ ggplot() +
   coord_fixed(1.3)
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 total_thing$Density <- as.numeric(as.character(total_thing$Density))
 
 
-## ---------------------------------------------------------------------------------------------------------------
+
 ggplot() + 
   geom_polygon(data = total_thing, aes(x=long, y = lat, fill=Density, group=County), 
                color = "#f8f8fa", size = 0.25, show.legend = T) +
@@ -448,7 +425,6 @@ ggplot() +
         legend.title.align = 0.5,
         legend.spacing.x = unit(0.5, "cm"), 
         legend.spacing.y = unit(0.5, "cm"),
-        # legend.box.margin = unit(c(0,0,0,0),"cm"),
         legend.background = element_rect(linetype = 0, size = 0.5, colour = 1),
         panel.background = element_blank(),
         axis.title.x = element_blank(),
@@ -459,4 +435,85 @@ ggplot() +
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
   coord_fixed(1.3)
+
+
+
+library(tidycensus)
+library(tidyverse)
+library(DT)
+library(tools)
+library(leaflet) 
+library(sf) 
+library(ggthemes)
+library(wesanderson)
+options(tigris_use_cache = TRUE) # Keeps map shapefiles on your hard drive rather than having to download them each time
+
+
+
+mon <- get_acs(state = "WV", 
+               county = "Monongalia", 
+               geography = "tract", 
+               variables = "B19013_001", 
+               geometry = TRUE)
+
+
+
+datatable(mon)
+
+
+wes_palette("Rushmore1")
+
+
+
+pal <- wes_palette("Rushmore1", 
+                   max(mon$estimate), 
+                   type = "continuous")
+
+
+
+mon %>%
+  ggplot(aes(fill = estimate)) + 
+  geom_sf(color = NA) + 
+  scale_fill_gradientn(colors = pal) +
+  theme_map() 
+
+
+
+racevars <- c(White = "P005003", 
+              Black = "P005004", 
+              Asian = "P005006", 
+              Hispanic = "P004003")
+
+
+
+mon_race <- get_decennial(geography = "tract",
+                          variables = racevars,
+                          state = "WV", 
+                          county = "Monongalia County", 
+                          geometry = TRUE,
+                          summary_var = "P001001") 
+
+
+
+datatable(mon_race)
+
+
+mon_race %>%
+  mutate(pct = 100 * (value / summary_value)) %>%
+  ggplot(aes(fill = pct)) +
+  facet_wrap(~variable) +
+  geom_sf(color = NA) + 
+  scale_fill_gradientn(colors = pal) +
+  theme_map() 
+
+
+
+mon_race %>%
+  mutate(pct = 100 * (value / summary_value)) %>%
+  ggplot(aes(fill = pct)) +
+  facet_wrap(~variable) +
+  geom_sf(color = NA) + 
+  scale_fill_gradientn(colors = pal,
+                       trans="log10") +
+  theme_map() 
 
