b7b4c842a7bdfde23b35842348ba3d5c4054b89c
piechartssuck <>
2021-03-12 12:18:23 -0500

    edp693e

--- content/example/08-example.R
+++ content/example/08-example.R
@@ -70,7 +70,6 @@ library(igraph)
 library(ggraph)
 library(harrypotter)
 
-
 titles <- c("Philosopher's Stone", 
             "Chamber of Secrets", 
             "Prisoner of Azkaban",
@@ -79,31 +78,33 @@ titles <- c("Philosopher's Stone",
             "Half-Blood Prince",
             "Deathly Hallows")
 
-books <- list("philosophers_stone", 
-              "chamber_of_secrets", 
-              "prisoner_of_azkaban",
-              "goblet_of_fire", 
-              "order_of_the_phoenix", 
-              "half_blood_prince",
-              "deathly_hallows")
-  
+books <- list(philosophers_stone, 
+              chamber_of_secrets, 
+              prisoner_of_azkaban,
+              goblet_of_fire, 
+              order_of_the_phoenix, 
+              half_blood_prince,
+              deathly_hallows)
+
 series <- tibble()
 
 for(i in seq_along(titles)) {
-        
-        clean <- tibble(chapter = seq_along(books[[i]]),
-                        text = books[[i]]) %>%
-             unnest_tokens(word, text) %>%
-             mutate(book = titles[i]) %>%
-             select(book, everything())
-
-        series <- rbind(series, clean)
+  
+  clean <- tibble(chapter = seq_along(books[[i]]),
+                  text = books[[i]]) %>%
+    unnest_tokens(word, text) %>%
+    mutate(book = titles[i]) %>%
+    select(book, everything())
+  
+  series <- rbind(series, clean)
 }
 
 # set factor to keep books in order of publication
 series$book <- factor(series$book, levels = rev(titles))
 
 series
+
+
 
 
 
